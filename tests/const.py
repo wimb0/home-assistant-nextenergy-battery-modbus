bb8cd64430b2899132ea3b8075bba9df06a65485
wimb0 <11030068+wimb0@users.noreply.github.com>
2025-10-14 14:50:28 +0200

    Create const.py

---
+++ tests/const.py
@@ -0,0 +1,9 @@
+"""Constants for NextEnergy Battery tests."""
+
+MOCK_CONFIG = {
+    "host": "1.2.3.4",
+    "port": 502,
+    "slave_id": 1,
+    "polling_interval": 30,
+    "prefix": "test_prefix",
+}
