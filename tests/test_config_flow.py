b783d674ab84ecf4a1a474d476578b66ee42a014
wimb0 <11030068+wimb0@users.noreply.github.com>
2025-10-16 17:26:50 +0200

    Refactor test_form_success and test_options_flow

--- tests/test_config_flow.py
+++ tests/test_config_flow.py
@@ -1,7 +1,5 @@
 """Test NextEnergy Battery config flow."""
 from unittest.mock import patch
-
-import pytest
 from homeassistant import config_entries
 from homeassistant.core import HomeAssistant
 from homeassistant.data_entry_flow import FlowResultType
@@ -10,26 +8,18 @@ from pytest_homeassistant_custom_component.common import MockConfigEntry
 from custom_components.nextenergy_battery.const import DOMAIN
 from .const import MOCK_CONFIG
 
-
-@pytest.mark.parametrize("user_input", [MOCK_CONFIG])
-async def test_form_success(hass: HomeAssistant, user_input, mock_modbus_client) -> None:
+async def test_form_success(hass: HomeAssistant, bypass_get_data) -> None:
     """Test we get the form and it works."""
-    result = await hass.config_entries.flow.async_init(
-        DOMAIN, context={"source": config_entries.SOURCE_USER}
-    )
+    result = await hass.config_entries.flow.async_init(DOMAIN, context={"source": config_entries.SOURCE_USER})
     assert result["type"] == FlowResultType.FORM
 
     with patch("custom_components.nextenergy_battery.async_setup_entry", return_value=True):
-        result2 = await hass.config_entries.flow.async_configure(
-            result["flow_id"],
-            user_input,
-        )
+        result2 = await hass.config_entries.flow.async_configure(result["flow_id"], MOCK_CONFIG)
         await hass.async_block_till_done()
 
     assert result2["type"] == FlowResultType.CREATE_ENTRY
 
-
-async def test_options_flow(hass: HomeAssistant, mock_modbus_client) -> None:
+async def test_options_flow(hass: HomeAssistant, bypass_get_data) -> None:
     """Test the options flow."""
     entry = MockConfigEntry(domain=DOMAIN, data=MOCK_CONFIG, options=MOCK_CONFIG)
     entry.add_to_hass(hass)
