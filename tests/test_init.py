d405a2d692c99730a47265cadab455ed7307b9be
wimb0 <11030068+wimb0@users.noreply.github.com>
2025-10-16 17:19:33 +0200

    Refactor test_setup_unload_and_reload_entry function

--- tests/test_init.py
+++ tests/test_init.py
@@ -1,24 +1,27 @@
 """Test NextEnergy Battery setup process."""
-from homeassistant.core import HomeAssistant
 from homeassistant.config_entries import ConfigEntryState
-
+from homeassistant.core import HomeAssistant
 from pytest_homeassistant_custom_component.common import MockConfigEntry
 
 from custom_components.nextenergy_battery.const import DOMAIN
 from .const import MOCK_CONFIG
 
-async def test_setup_unload_and_reload_entry(hass: HomeAssistant, bypass_get_data):
+
+async def test_setup_unload_and_reload_entry(hass: HomeAssistant, mock_modbus_client):
     """Test entry setup and unload."""
+    # Create a mock config entry
     entry = MockConfigEntry(domain=DOMAIN, data=MOCK_CONFIG, options=MOCK_CONFIG)
     entry.add_to_hass(hass)
 
+    # Set up the entry and assert that it's loaded
     await hass.config_entries.async_setup(entry.entry_id)
     await hass.async_block_till_done()
 
     assert entry.state == ConfigEntryState.LOADED
+    assert DOMAIN in hass.data
 
+    # Unload the entry and assert that it's been removed
     assert await hass.config_entries.async_unload(entry.entry_id)
     await hass.async_block_till_done()
 
     assert entry.state == ConfigEntryState.NOT_LOADED
-
